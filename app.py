7c4701bf249125ac65b32258aae413ec6837c939
geosa <sakhabutdinovga@yandex.ru>
2022-09-20 19:10:16 +0500

    add compose webhook

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import json
 import os
 from flask import Flask, request, jsonify, Response
+from python_on_whales import DockerClient
 
 from docker_utils import docker_handler
 from handlers import process_request, get_image_name
 from logger import init_logger
 
 app = Flask(__name__)
-AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+# AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+AUTH_TOKEN = "557e2d4f-064a-43d2-a8c7-5a9da8cf07fc"
 init_logger('deploy_service')
 
 
@@ -34,6 +36,32 @@ def process_webhook() -> tuple[Response, int]:
     image_name, container_name = get_image_name(owner=owner, repository=repository, tag=tag)
     docker_handler.start(image_name=image_name, container_name=container_name, ports=ports, path=path, build=build)
     return jsonify({'message': 'Deploy complete'}), 200
+
+
+@app.route('/compose', methods=['POST'])
+def process_compose_webhook() -> tuple[Response, int]:
+    if request.headers.get('Authorization', '') != AUTH_TOKEN:
+        return jsonify({'message': 'Bad token'}), 401
+
+    try:
+        request_data = json.loads(request.data)
+    except json.JSONDecodeError:
+        return jsonify({'message': 'Bad request'}), 401
+
+    path = request_data.get('path')
+    file = request_data.get('file')
+
+    if path is None or file is None:
+        return jsonify({'message': 'Bad request'}), 401
+
+    full_filename = os.path.join(path, file)
+    try:
+        docker = DockerClient(compose_files=[full_filename])
+        docker.compose.build()
+        docker.compose.up(detach=True)
+        return jsonify({'message': 'Deploy complete'}), 200
+    except Exception:
+        return jsonify({'message': 'Deploy error'}), 500
 
 
 if __name__ == '__main__':
